b5a800bcb228c5491b2d78c6c0e5538f32074351
Poverd <pierreallaire@outlook.com>
2023-01-05 02:13:20 +0100

    Version finale de l'app

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var button = document.querySelector('.generer');
 var boite = document.querySelector('.boite');
 
 button.onclick = function(){
-var url = 'https://api.adviceslip.com/advice'; //Nous avons mis dans une variable url le lien de notre API ;
+var url = 'https://api.adviceslip.com/advice';
     
 fetch(url).then((response)=>
 response.json().then((data)=>
